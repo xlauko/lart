9643acdc5a2cf083501144fb6b7ee990916a1370
Henrich Lauko <xlauko@mail.muni.cz>
2020-08-15 14:14:22 +0200

    cc: Setup single pass lart.

---
+++ lib/cc/pass.cpp
@@ -0,0 +1,42 @@
+/*
+ * (c) 2020 Henrich Lauko <xlauko@mail.muni.cz>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "pass.hpp"
+
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+
+namespace lart
+{
+    bool pass::runOnModule( llvm::Module & )
+    {
+        return true;
+    }
+
+    void regiter_pass( const llvm::PassManagerBuilder &, llvm::legacy::PassManagerBase &pm )
+    {
+        pm.add( new lart::pass() );
+    }
+
+    char pass::ID = 0;
+
+} // namespace lart
+
+static llvm::RegisterPass< lart::pass > X( "lart", "Abstraction Pass" );
+static struct llvm::RegisterStandardPasses Y( llvm::PassManagerBuilder::EP_VectorizerStart,
+                                              lart::regiter_pass );
+static struct llvm::RegisterStandardPasses Z( llvm::PassManagerBuilder::EP_EnabledOnOptLevel0,
+                                              lart::regiter_pass );
