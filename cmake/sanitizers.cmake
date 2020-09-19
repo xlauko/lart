0a46c5186d0874524df0bd3eea36fbe5f8419a8d
Henrich Lauko <xlauko@mail.muni.cz>
2020-09-20 01:18:40 +0200

    cmkae: fix name clashes

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -5,29 +5,29 @@ function( enable_sanitizers project_name )
     option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
 
     if(ENABLE_COVERAGE)
-      target_compile_options(project_options INTERFACE --coverage -O0 -g)
-      target_link_libraries(project_options INTERFACE --coverage)
+      target_compile_options( lart_project_options INTERFACE --coverage -O0 -g)
+      target_link_libraries( lart_project_options INTERFACE --coverage)
     endif()
 
     set(SANITIZERS "")
 
-    option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" FALSE)
+    option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ON)
     if(ENABLE_SANITIZER_ADDRESS)
       list(APPEND SANITIZERS "address")
     endif()
 
-    option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" FALSE)
+    option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
     if(ENABLE_SANITIZER_MEMORY)
       list(APPEND SANITIZERS "memory")
     endif()
 
     option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR
-           "Enable undefined behavior sanitizer" FALSE)
+           "Enable undefined behavior sanitizer" ON)
     if(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR)
       list(APPEND SANITIZERS "undefined")
     endif()
 
-    option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" FALSE)
+    option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
     if(ENABLE_SANITIZER_THREAD)
       list(APPEND SANITIZERS "thread")
     endif()
