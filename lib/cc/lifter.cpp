1fcf93d09177b5ef6749c24cc30fa43a25ec8b26
Henrich Lauko <xlauko@mail.muni.cz>
2021-01-25 21:45:47 +0100

    cc: generate lifter declarations

---
+++ lib/cc/lifter.cpp
@@ -0,0 +1,57 @@
+/*
+ * (c) 2021 Henrich Lauko <xlauko@mail.muni.cz>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <cc/lifter.hpp>
+
+#include <sc/ranges.hpp>
+
+namespace lart
+{
+    namespace sv = sc::views;
+
+    std::string lifter::name() const
+    {
+        return "lart.lifter." + op::name(op) + "." + op::unique_name_suffix(op);
+    }
+
+    llvm::Function* lifter::function() const
+    {
+        auto get_or_insert_function = [&] (auto fty, auto name) {
+            auto fn = llvm::cast< llvm::Function >(
+                module.getOrInsertFunction( name, fty ).getCallee()
+            );
+            fn->addFnAttr( llvm::Attribute::NoUnwind );
+            return fn;
+        };
+
+        std::vector< llvm::Value * > args;
+        for ( auto arg : op::arguments(op) ) {
+            if ( arg.liftable ) {
+                args.push_back(sc::i1(false)); // dummy false
+                args.push_back(arg.value);
+                args.push_back(op::abstract_pointer());
+            } else {
+                args.push_back(arg.value);
+            }
+        }
+
+        auto out = op::default_value(op);
+        auto fty = llvm::FunctionType::get( out->getType(), sv::freeze( args | sv::types ), false );
+
+        return get_or_insert_function( fty, name() );;
+    }
+
+} // namespace lart
