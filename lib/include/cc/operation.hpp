4e5aecf2589ca9b23b6ae474b9f20ff1e703bfb1
Henrich Lauko <xlauko@mail.muni.cz>
2021-01-25 12:45:59 +0100

    cc: add argument getters to operations

--- lib/include/cc/operation.hpp
+++ lib/include/cc/operation.hpp
@@ -19,7 +19,9 @@
 #include <cc/dfa.hpp>
 
 #include <llvm/IR/Value.h>
+
 #include <variant>
+#include <experimental/iterator>
 namespace lart::op
 {
     struct base
@@ -38,34 +40,76 @@ namespace lart::op
         llvm::Instruction * _where;
     };
 
+    struct argument
+    {
+        llvm::Value * value;
+        bool liftable;
+
+        template< typename stream >
+        friend auto operator<<( stream &s, argument a ) -> decltype( s << "" )
+        {
+            auto lift = a.liftable ? "lift" : "concrete";
+            return s << sc::fmt::llvm_name( a.value ) << ":" << lift;
+        }
+    };
+
+    static constexpr bool lift = true;
+    static constexpr bool concrete = !lift;
+
+    using args_t = std::vector< argument >;
+
     struct melt : base
     {
         std::string name() const { return "melt"; }
+        args_t arguments() const
+        {
+            auto load = llvm::cast< llvm::LoadInst >( _what );
+            return { { load->getPointerOperand(), concrete } };
+        }
     };
 
     struct freeze : base
     {
         std::string name() const { return "freeze"; }
+        args_t arguments() const
+        {
+            auto store = llvm::cast< llvm::StoreInst >( _what );
+            return {
+                { store->getValueOperand(), lift },
+                { store->getPointerOperand(), concrete }
+            };
+        }
     };
 
     struct binary : base
     {
         std::string name() const { return "bin"; }
+        args_t arguments() const
+        {
+            auto bin = llvm::cast< llvm::BinaryOperator >( _what );
+            return {
+                { bin->getOperand( 0 ), lift },
+                { bin->getOperand( 1 ), lift },
+            };
+        }
     };
 
     struct alloc : base
     {
         std::string name() const { return "alloca"; }
+        args_t arguments() const { return {}; }
     };
 
     struct store : base
     {
         std::string name() const { return "store"; }
+        args_t arguments() const { return {}; }
     };
 
     struct load : base
     {
         std::string name() const { return "load"; }
+        args_t arguments() const { return {}; }
     };
 
     using operation = std::variant<
@@ -78,7 +122,17 @@ namespace lart::op
         static auto invoke = [] (auto f) { return [=] ( auto a ) { return std::visit(f, a); }; };
     } // namespace detail
 
-    static auto location = detail::invoke( [] (auto o) { return o.location(); } );
-    static auto name = detail::invoke( [] (const auto &o) { return o.name(); } );
+    static auto location  = detail::invoke( [] (auto o) { return o.location(); } );
+    static auto name      = detail::invoke( [] (const auto &o) { return o.name(); } );
+    static auto arguments = detail::invoke( [] (const auto &o) { return o.arguments(); } );
+
+    template< typename stream >
+    auto operator<<( stream &s, const args_t &a ) -> decltype( s << "" )
+    {
+        s << '[';
+        std::copy(a.begin(), a.end(), std::experimental::make_ostream_joiner(s, ", "));
+        s << "]";
+        return s;
+    }
 
 } // namespace lart::op
