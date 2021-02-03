996c62b2f30a8ef89bd1acc8ffcaf2ed61765de8
Henrich Lauko <xlauko@mail.muni.cz>
2021-02-03 19:04:44 +0100

    rt: add lart interface file

---
+++ lib/include/runtime/lart.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+   /* Abstract versions of arguments and return values are passed in a 'stash', a
+    * global (per-thread) location. On 'unstash', we clear the stash for two
+    * reasons: 1) it flags up otherwise hard-to-pin bugs where mismatched
+    * stash/unstash reads an old value of the stash, and 2) when verification
+    * encounters a possible diamond (the explicit content of the state is the
+    * same, e.g.  after an if/else) and we leave old stashes around, it might so
+    * happen that the stashed values in the candidate states are of different
+    * types, leading to a type mismatch in the comparison.
+    *
+    * TODO The second problem might have to be tackled in the verification
+    * algorithm anyway, since there might be other ways to trigger the same
+    * problem. */
+    void __lart_stash( void * );
+    void * __lart_unstash();
+
+#ifdef __cplusplus
+}
+#endif
