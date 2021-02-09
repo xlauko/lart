35f12a6a0a030c3e9998ffdde79b3c23122ba7f8
Henrich Lauko <xlauko@mail.muni.cz>
2021-02-09 22:12:13 +0100

    lava: add base domain

---
+++ lib/include/lava/support/base.hpp
@@ -0,0 +1,29 @@
+/*
+ * (c) 2019 Henrich Lauko <xlauko@mail.muni.cz>
+ * (c) 2020 Petr Ročkai <code@fixp.eu>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <cstdint>
+
+#include <lava/support/array.hpp>
+
+namespace __lava
+{
+    using bitwidth_t = uint8_t;
+    using tag_t = uint8_t;
+
+    struct base {};
+
+} // namespace __lava
