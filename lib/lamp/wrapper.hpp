83ad0b783f027ee1ff24d5fe720a4d471d13a5b7
Henrich Lauko <xlauko@mail.muni.cz>
2021-02-10 22:31:08 +0100

    lamp: add wrapper

--- lib/lamp/wrapper.hpp
+++ lib/lamp/wrapper.hpp
@@ -19,3 +19,38 @@
  * it implements the LART-facing metadomain wrapper. Each metadomain should be
  * compiled into a separate bitcode file which contains exactly one copy
  * (build) of this file. See e.g. trivial.cpp for an example. */
+
+#include <lamp/lamp.h>
+
+#include <lava/support/base.hpp> /* iN */
+#include <lava/constant.hpp>
+
+using dom = __lamp::meta_domain;
+using bw = __lava::bitwidth_t;
+
+using namespace __lava; /* iN */
+
+using ref = domain_ref< dom >;
+
+struct wrapper
+{
+    template< typename op_t >
+    __inline static __lamp_ptr wrap( op_t op ) { return { op().disown() }; }
+
+    template< typename op_t, typename... args_t >
+    __inline static __lamp_ptr wrap( op_t op, __lamp_ptr arg, args_t... args )
+    {
+        ref a( arg.ptr );
+        return wrap( [&]( const auto & ... args_ ) __inline { return op( a, args_... ); }, args... );
+    }
+
+    template< typename op_t, typename arg_t, typename... args_t >
+    __inline static auto wrap( op_t op, const arg_t &arg, args_t... args )
+        -> std::enable_if_t< !std::is_same_v< arg_t, dom >, __lamp_ptr >
+    {
+        return wrap( [&] ( const auto & ... args_ ) __inline { return op( arg, args_... ); }, args... );
+    }
+};
+
+template< typename... args_t >
+__inline static auto wrap( const args_t & ... args ) { return wrapper::wrap( args... ); }
