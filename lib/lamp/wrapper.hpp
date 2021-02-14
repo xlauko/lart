21cb2f850bc79ada1477f16433318d12d327d3fe
Henrich Lauko <xlauko@mail.muni.cz>
2021-02-14 20:53:41 +0100

    lamp: do not include lava stubs

--- lib/lamp/wrapper.hpp
+++ lib/lamp/wrapper.hpp
@@ -20,10 +20,10 @@
  * compiled into a separate bitcode file which contains exactly one copy
  * (build) of this file. See e.g. trivial.cpp for an example. */
 
-#include <lamp/lamp.h>
-
 #include <lava/support/base.hpp> /* iN */
 #include <lava/constant.hpp>
+
+typedef struct { void *ptr; } __lamp_ptr;
 
 using dom = __lamp::meta_domain;
 using bw = __lava::bitwidth_t;
@@ -54,3 +54,186 @@ struct wrapper
 
 template< typename... args_t >
 __inline static auto wrap( const args_t & ... args ) { return wrapper::wrap( args... ); }
+
+template< typename type >
+inline type defualt_tainted_value() {
+    uint64_t value = 0;
+    __lart_set_taint( &value, sizeof( value ) );
+    if constexpr ( std::is_pointer_v< type > )
+        return reinterpret_cast< type >( value );
+    else
+        return static_cast< type >( value );
+}
+
+template< typename op_t, typename arg_t, typename... args_t >
+static arg_t lift( op_t op, arg_t arg, args_t... args )
+{
+    __lart_stash( op( arg, args... ).disown() );
+    __lart_set_taint( &arg, sizeof( arg ) );
+    return arg;
+}
+
+template< typename dom, typename type >
+static auto any()
+{
+    __lart_stash( dom::template any< type >().disown() );
+    return defualt_tainted_value< type >();
+}
+
+template< typename T >
+static T lower( __lamp_ptr v )
+{
+    return cast< false /* unsigned */, bitwidth_v< T > >( dom::lower( ref( v.ptr ) )->value );
+}
+
+
+extern "C"
+{
+    i1  __lamp_lift_i1 ( i1  v )     { return lift( dom::lift_i1,  v ); }
+    i8  __lamp_lift_i8 ( i8  v )     { return lift( dom::lift_i8,  v ); }
+    i16 __lamp_lift_i16( i16 v )     { return lift( dom::lift_i16, v ); }
+    i32 __lamp_lift_i32( i32 v )     { return lift( dom::lift_i32, v ); }
+    i64 __lamp_lift_i64( i64 v )     { return lift( dom::lift_i64, v ); }
+
+    si8  __lamp_lift_si8 ( si8  v )  { return lift( dom::lift_si8,  v ); }
+    si16 __lamp_lift_si16( si16 v )  { return lift( dom::lift_si16, v ); }
+    si32 __lamp_lift_si32( si32 v )  { return lift( dom::lift_si32, v ); }
+    si64 __lamp_lift_si64( si64 v )  { return lift( dom::lift_si64, v ); }
+
+    f32 __lamp_lift_f32( f32 v )     { return lift( dom::lift_f32, v ); }
+    f64 __lamp_lift_f64( f64 v )     { return lift( dom::lift_f64, v ); }
+
+    __lamp_ptr __lamp_wrap_i1 ( i1  v )     { return wrap( dom::lift_i1,  v ); }
+    __lamp_ptr __lamp_wrap_i8 ( i8  v )     { return wrap( dom::lift_i8,  v ); }
+    __lamp_ptr __lamp_wrap_i16( i16 v )     { return wrap( dom::lift_i16, v ); }
+    __lamp_ptr __lamp_wrap_i32( i32 v )     { return wrap( dom::lift_i32, v ); }
+    __lamp_ptr __lamp_wrap_i64( i64 v )     { return wrap( dom::lift_i64, v ); }
+
+    __lamp_ptr __lamp_wrap_si8 ( si8  v )     { return wrap( dom::lift_si8,  v ); }
+    __lamp_ptr __lamp_wrap_si16( si16 v )     { return wrap( dom::lift_si16, v ); }
+    __lamp_ptr __lamp_wrap_si32( si32 v )     { return wrap( dom::lift_si32, v ); }
+    __lamp_ptr __lamp_wrap_si64( si64 v )     { return wrap( dom::lift_si64, v ); }
+
+    __lamp_ptr __lamp_wrap_f32( f32 v )     { return wrap( dom::lift_f32, v ); }
+    __lamp_ptr __lamp_wrap_f64( f64 v )     { return wrap( dom::lift_f64, v ); }
+
+    __lamp_ptr  __lamp_wrap_ptr( void *v )   { return wrap( dom::lift_ptr, v ); }
+     void *__lamp_lift_ptr( void *v ) { return lift( dom::lift_ptr, v ); }
+
+     void *__lamp_lift_arr( void *v, i32 s ) { return lift( dom::lift_arr, v, s ); }
+
+    i1  __lamp_lower_i1 ( __lamp_ptr v ) { return lower<  i1 >( v ); }
+    i8  __lamp_lower_i8 ( __lamp_ptr v ) { return lower<  i8 >( v ); }
+    i16 __lamp_lower_i16( __lamp_ptr v ) { return lower< i16 >( v ); }
+    i32 __lamp_lower_i32( __lamp_ptr v ) { return lower< i32 >( v ); }
+    i64 __lamp_lower_i64( __lamp_ptr v ) { return lower< i64 >( v ); }
+
+    i8    __lamp_any_i8()    { return any< dom, i8  >(); }
+    i16   __lamp_any_i16()   { return any< dom, i16 >(); }
+    i32   __lamp_any_i32()   { return any< dom, i32 >(); }
+    i64   __lamp_any_i64()   { return any< dom, i64 >(); }
+
+    f32   __lamp_any_f32()   { return any< dom, f32 >(); }
+    f64   __lamp_any_f64()   { return any< dom, f64 >(); }
+
+     void *__lamp_any_ptr() { return any< dom, void * >(); }
+     char *__lamp_any_array() { return any< dom, char * >(); }
+
+     __lamp_ptr __lamp_alloca( __lamp_ptr size, bw w )
+    {
+        return wrap( []( const auto &... x ) { return dom::op_alloca( x... ); }, size, w );
+    }
+
+    /*8void __lamp_freeze( void *val, void *addr, bw size )
+    {
+        __lamp_freeze_impl( val, addr, size );
+    }
+
+    __lamp_ptr __lamp_melt( void *addr, bw size )
+    {
+        return { __lamp_melt_impl( addr, size ).disown() };
+    }*/
+
+    __lamp_ptr __lamp_join( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_join, a, b ); }
+    __lamp_ptr __lamp_meet( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_meet, a, b ); }
+
+    __lamp_ptr __lamp_add ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_add, a, b ); }
+    __lamp_ptr __lamp_sub ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_sub, a, b ); }
+    __lamp_ptr __lamp_mul ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_mul, a, b ); }
+    __lamp_ptr __lamp_sdiv( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_sdiv, a, b ); }
+    __lamp_ptr __lamp_udiv( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_udiv, a, b ); }
+    __lamp_ptr __lamp_srem( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_srem, a, b ); }
+    __lamp_ptr __lamp_urem( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_urem, a, b ); }
+
+    __lamp_ptr __lamp_fadd( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fadd, a, b ); }
+    __lamp_ptr __lamp_fsub( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fsub, a, b ); }
+    __lamp_ptr __lamp_fmul( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fmul, a, b ); }
+    __lamp_ptr __lamp_fdiv( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fdiv, a, b ); }
+    __lamp_ptr __lamp_frem( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_frem, a, b ); }
+
+    __lamp_ptr __lamp_shl ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_shl,  a, b ); }
+    __lamp_ptr __lamp_ashr( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_ashr, a, b ); }
+    __lamp_ptr __lamp_lshr( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_lshr, a, b ); }
+    __lamp_ptr __lamp_and ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_and,  a, b ); }
+    __lamp_ptr __lamp_or  ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_or,   a, b ); }
+    __lamp_ptr __lamp_xor ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_xor,  a, b ); }
+
+    __lamp_ptr __lamp_eq ( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_eq,  a, b ); }
+    __lamp_ptr __lamp_ne ( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_ne,  a, b ); }
+    __lamp_ptr __lamp_ugt( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_ugt, a, b ); }
+    __lamp_ptr __lamp_uge( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_uge, a, b ); }
+    __lamp_ptr __lamp_ult( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_ult, a, b ); }
+    __lamp_ptr __lamp_ule( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_ule, a, b ); }
+    __lamp_ptr __lamp_sgt( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_sgt, a, b ); }
+    __lamp_ptr __lamp_sge( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_sge, a, b ); }
+    __lamp_ptr __lamp_slt( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_slt, a, b ); }
+    __lamp_ptr __lamp_sle( __lamp_ptr a, __lamp_ptr b )  { return wrap( dom::op_sle, a, b ); }
+
+    __lamp_ptr __lamp_foeq( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_foeq, a, b ); }
+    __lamp_ptr __lamp_fogt( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fogt, a, b ); }
+    __lamp_ptr __lamp_foge( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_foge, a, b ); }
+    __lamp_ptr __lamp_folt( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_folt, a, b ); }
+    __lamp_ptr __lamp_fole( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fole, a, b ); }
+    __lamp_ptr __lamp_fone( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fone, a, b ); }
+    __lamp_ptr __lamp_ford( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_ford, a, b ); }
+    __lamp_ptr __lamp_funo( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_funo, a, b ); }
+    __lamp_ptr __lamp_fueq( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fueq, a, b ); }
+    __lamp_ptr __lamp_fugt( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fugt, a, b ); }
+    __lamp_ptr __lamp_fuge( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fuge, a, b ); }
+    __lamp_ptr __lamp_fult( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fult, a, b ); }
+    __lamp_ptr __lamp_fule( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fule, a, b ); }
+    __lamp_ptr __lamp_fune( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_fune, a, b ); }
+
+    __lamp_ptr __lamp_ffalse( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_ffalse, a, b ); }
+    __lamp_ptr __lamp_ftrue ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_ftrue,  a, b ); }
+
+    __lamp_ptr __lamp_concat ( __lamp_ptr a, __lamp_ptr b ) { return wrap( dom::op_concat,  a, b ); }
+    __lamp_ptr __lamp_trunc  ( __lamp_ptr a, bw  b ) { return wrap( dom::op_trunc,   a, b ); }
+    __lamp_ptr __lamp_fptrunc( __lamp_ptr a, bw  b ) { return wrap( dom::op_fptrunc, a, b ); }
+    __lamp_ptr __lamp_sitofp ( __lamp_ptr a, bw  b ) { return wrap( dom::op_sitofp,  a, b ); }
+    __lamp_ptr __lamp_uitofp ( __lamp_ptr a, bw  b ) { return wrap( dom::op_uitofp,  a, b ); }
+    __lamp_ptr __lamp_zext   ( __lamp_ptr a, bw  b ) { return wrap( dom::op_zext,    a, b ); }
+    __lamp_ptr __lamp_zfit   ( __lamp_ptr a, bw  b ) { return wrap( dom::op_zfit,    a, b ); }
+    __lamp_ptr __lamp_sext   ( __lamp_ptr a, bw  b ) { return wrap( dom::op_sext,    a, b ); }
+    __lamp_ptr __lamp_fpext  ( __lamp_ptr a, bw  b ) { return wrap( dom::op_fpext,   a, b ); }
+    __lamp_ptr __lamp_fptosi ( __lamp_ptr a, bw  b ) { return wrap( dom::op_fptosi,  a, b ); }
+    __lamp_ptr __lamp_fptoui ( __lamp_ptr a, bw  b ) { return wrap( dom::op_fptoui,  a, b ); }
+
+    __lamp_ptr __lamp_store( __lamp_ptr a, __lamp_ptr b, bw w )
+    {
+        return wrap( []( const auto &... x ) { return dom::op_store( x... ); }, a, b, w );
+    }
+
+    __lamp_ptr __lamp_load( __lamp_ptr a, bw w ) { return wrap( dom::op_load, a, w ); }
+
+    uint8_t __lamp_to_tristate( __lamp_ptr v )
+    {
+        return dom::to_tristate( ref( v.ptr ) ).value;
+    }
+
+    __lamp_ptr __lamp_assume( __lamp_ptr a, bool c ) { return wrap( dom::assume, a, c ); }
+    __lamp_ptr __lamp_extract( __lamp_ptr a, bw s, bw e ) { return wrap( dom::op_extract, a, s, e ); }
+    bool __lamp_decide( uint8_t tr ) { return __lava::lower( __lava::tristate( tr ) ); }
+
+    //void __lamp_dealloca( void * addr, uint64_t size ) { __lamp_dealloca_impl( addr, size ); }
+}
