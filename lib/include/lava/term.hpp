ff5b9a44723da060617c86bc0ee997875dc27b89
Henrich Lauko <xlauko@mail.muni.cz>
2021-03-17 11:14:15 +0100

    lava: import simple symbolic domain

--- lib/include/lava/term.hpp
+++ lib/include/lava/term.hpp
@@ -22,13 +22,15 @@
 
 #include <set>
 #include <stdio.h>
+#include <type_traits>
 #include <z3++.h>
+
+#include <iostream>
 
 using namespace std::string_literals;
 
 namespace __lava
 {
-    using term_storage = tagged_storage< z3::expr >;
     
     struct term_state_t
     {
@@ -47,6 +49,16 @@ namespace __lava
     }
 
     term_state_t state;
+    
+    struct z3_expr { 
+        z3_expr( z3::expr e ) : expr( e ) {}
+        /*{
+            printf("assert: %s\n", Z3_ast_to_string(state.ctx, e));
+        }*/
+        z3::expr expr;
+    };
+
+    using term_storage = tagged_storage< z3_expr >;
 
     struct term : term_storage, domain_mixin< term >
     {
@@ -55,85 +67,113 @@ namespace __lava
 
         using tr = const term &;
   
-        term() : term_storage( state.ctx ) {}
+        term( z3::expr e ) : term_storage( z3_expr{ e } ) {}
         term( void *v, construct_shared_t s ) : term_storage( v, s ) {}
 
-        template< typename type > static term lift( type ) { return z3::expr( state.ctx ); }
-        template< typename type > static term any()
+        template< typename type > static term lift( type value )
         {
+            auto &ctx = state.ctx;
+
+            if constexpr ( std::is_same_v< type, bool > )
+            {
+                return ctx.bool_val( value );
+            }
+
             if constexpr ( std::is_integral_v < type > )
             {
-                constexpr auto digits = std::numeric_limits< type >::digits;
-                char name[5 + digits] = {};
-                std::strncpy( name, "var_", 4 );
-                std::sprintf( name + 4, "%d", variable_counter() );
-                return state.ctx.bv_const( name, bitwidth_v< type > );
+                return ctx.bv_val( value, bitwidth_v< type > );
+            }
+
+            __builtin_unreachable();
+        }
+
+        template< typename type > static term any()
+        {
+            constexpr auto digits = std::numeric_limits< type >::digits;
+            char name[5 + digits] = {};
+            std::strncpy( name, "var_", 4 );
+            std::sprintf( name + 4, "%d", variable_counter() );
+
+            auto &ctx = state.ctx;            
+            if constexpr ( std::is_integral_v < type > )
+            {
+                return ctx.bv_const( name, bitwidth_v< type > );
+            }
+
+            if constexpr ( std::is_floating_point_v< type > )
+            {
+                if constexpr ( bitwidth_v< type > == 32 )
+                    return ctx.fpa_const( name, 8, 24 );
+                if constexpr ( bitwidth_v< type > == 64 )
+                    return ctx.fpa_const( name, 11, 53 );
             }
             __builtin_unreachable();
         }
 
-        static constant lower( tr ) { return {}; };
+        static constant lower( tr ) { __builtin_unreachable(); };
 
-        static void assume( tr, bool ) {}
+        static void assume( tr t, bool expected ) 
+        {
+            auto &solver = state.solver;
+            solver.add( expected ? t->expr : !t->expr );
+            
+            if ( solver.check() == z3::unsat ) {
+                __lart_cancel();
+            }
+        }
 
         static tristate to_tristate( tr ) { return maybe; }
 
         /* arithmetic operations */
-        static term op_add ( tr, tr ) { return {}; }
-        static term op_fadd( tr, tr ) { return {}; }
-        static term op_sub ( tr, tr ) { return {}; }
-        static term op_fsub( tr, tr ) { return {}; }
-        static term op_mul ( tr, tr ) { return {}; }
-        static term op_fmul( tr, tr ) { return {}; }
-        static term op_udiv( tr, tr ) { return {}; }
-        static term op_sdiv( tr, tr ) { return {}; }
-        static term op_fdiv( tr, tr ) { return {}; }
-        static term op_urem( tr, tr ) { return {}; }
-        static term op_srem( tr, tr ) { return {}; }
-        static term op_frem( tr, tr ) { return {}; }
-
-        static term op_fneg( tr ) { return {}; }
+        static term op_add ( tr a, tr b ) { return a->expr + b->expr; }
+        static term op_sub ( tr a, tr b ) { return a->expr - b->expr; }
+        static term op_mul ( tr a, tr b ) { return a->expr * b->expr; }
+        static term op_udiv( tr a, tr b ) { return z3::udiv( a->expr, b->expr ); }
+        static term op_sdiv( tr a, tr b ) { return a->expr / b->expr; }
+        static term op_urem( tr a, tr b ) { return z3::urem( a->expr, b->expr ); }
+        static term op_srem( tr a, tr b ) { return z3::srem( a->expr, b->expr ); }
 
         /* bitwise operations */
-        static term op_shl ( tr, tr ) { return {}; }
-        static term op_lshr( tr, tr ) { return {}; }
-        static term op_ashr( tr, tr ) { return {}; }
-        static term op_and ( tr, tr ) { return {}; }
-        static term op_or  ( tr, tr ) { return {}; }
-        static term op_xor ( tr, tr ) { return {}; }
+        static term op_shl ( tr a, tr b ) { return z3::shl( a->expr, b->expr ); }
+        static term op_lshr( tr a, tr b ) { return z3::lshr( a->expr, b->expr ); }
+        static term op_ashr( tr a, tr b ) { return z3::ashr( a->expr, b->expr ); }
+        static term op_and ( tr a, tr b ) { return a->expr & b->expr; }
+        static term op_or  ( tr a, tr b ) { return a->expr | b->expr; }
+        static term op_xor ( tr a, tr b ) { return a->expr ^ b->expr; }
 
         using bw = uint8_t;
 
         /* comparison operations */
-        static term op_eq ( tr, tr ) { return {}; }
-        static term op_ne ( tr, tr ) { return {}; }
-        static term op_ugt( tr, tr ) { return {}; }
-        static term op_uge( tr, tr ) { return {}; }
-        static term op_ult( tr, tr ) { return {}; }
-        static term op_ule( tr, tr ) { return {}; }
-        static term op_sgt( tr, tr ) { return {}; }
-        static term op_sge( tr, tr ) { return {}; }
-        static term op_slt( tr, tr ) { return {}; }
-        static term op_sle( tr, tr ) { return {}; }
+        static term op_eq ( tr a, tr b ) { return a->expr == b->expr; }
+        static term op_ne ( tr a, tr b ) { return a->expr != b->expr; }
+        static term op_ugt( tr a, tr b ) { return z3::ugt( a->expr, b->expr ); }
+        static term op_uge( tr a, tr b ) { return z3::uge( a->expr, b->expr ); }
+        static term op_ult( tr a, tr b ) { return z3::ult( a->expr, b->expr ); }
+        static term op_ule( tr a, tr b ) { return z3::ule( a->expr, b->expr ); }
+        static term op_sgt( tr a, tr b ) { return a->expr > b->expr; }
+        static term op_sge( tr a, tr b ) { return a->expr >= b->expr; }
+        static term op_slt( tr a, tr b ) { return a->expr < b->expr; }
+        static term op_sle( tr a, tr b ) { return a->expr <= b->expr; }
 
-        static term op_ffalse( tr, tr ) { return {}; }
-        static term op_ftrue( tr, tr ) { return {}; }
+        // static term op_inttoptr( tr, bw ) { return {}; }
+        // static term op_ptrtoint( tr, bw ) { return {}; }
+        static term op_sext( tr t, bw b ) { return z3::sext( t->expr, b ); }
+        // static term op_sitofp( tr, bw ) { return {}; }
+        static term op_trunc( tr t, bw b ) { return t->expr.extract( b - 1, 0 ); }
+        // static term op_uitofp( tr, bw ) { return {}; }
+        static term op_zext( tr t, bw b ) { return z3::zext( t->expr, b ); }
+        // static term op_zfit( tr t, bw ) { return {}; }
 
 
-        static term op_fpext( tr, bw ) { return {}; }
-        static term op_fptosi( tr, bw ) { return {}; }
-        static term op_fptoui( tr, bw ) { return {}; }
-        static term op_fptrunc( tr, bw ) { return {}; }
-        static term op_inttoptr( tr, bw ) { return {}; }
-        static term op_ptrtoint( tr, bw ) { return {}; }
-        static term op_sext( tr, bw ) { return {}; }
-        static term op_sitofp( tr, bw ) { return {}; }
-        static term op_trunc( tr, bw ) { return {}; }
-        static term op_uitofp( tr, bw ) { return {}; }
-        static term op_zext( tr, bw ) { return {}; }
-        static term op_zfit( tr, bw ) { return {}; }
+        static void dump( tr t )
+        {
+            printf( "%s\n", Z3_ast_to_string( state.ctx, t->expr ) );
+        }
 
-        static std::string trace( tr ) { return "term"; }
+        static std::string trace( tr t )
+        {
+            return Z3_ast_to_string( state.ctx, t->expr );
+        }
     };
 
 
