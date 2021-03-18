33c0bbd4803223f8981d22882db2cacfe780921e
Henrich Lauko <xlauko@mail.muni.cz>
2021-03-18 20:24:32 +0100

    test: add domain join test

---
+++ test/basic/join-a.c
@@ -0,0 +1,12 @@
+// RUN: %testrun %lartcc trivial %s -o %t | %filecheck %s
+// RUN: %testrun %lartcc unit %s -o %t | %filecheck %s
+
+#include <lamp.h>
+#include "utils.h"
+
+int main() {
+    uint8_t x = __lamp_any_i8();
+    uint8_t y = __lamp_lift_i8( 10 );
+    uint8_t z = x + y;
+    ASSERT( x != z ); // CHECK: assertion `x != y' failed
+}
