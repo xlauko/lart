d0c091a280923606168331c458cf4d8baf4cbcd7
Henrich Lauko <xlauko@mail.muni.cz>
2021-03-18 20:23:22 +0100

    lamp: add trivial example of semilattice

--- lib/lamp/trivial.cpp
+++ lib/lamp/trivial.cpp
@@ -18,12 +18,30 @@
 #include <lava/unit.hpp>
 #include <lava/constant.hpp>
 
+#include <lamp/support/semilattice.hpp>
+
 namespace __lamp
 {
     using namespace __lava;
 
     struct trivial
     {
+        using doms = domain_list< unit, constant >;
+
+        using scalar_lift_dom = constant;
+        using scalar_any_dom = unit;
+        using array_lift_dom = unit;
+        using array_any_dom = unit;
+
+        static constexpr int join( int a, int b ) noexcept
+        {
+            if ( a == doms::idx< constant > ) return b;
+            if ( b == doms::idx< constant > ) return a;
+
+            return doms::idx< unit >;
+        }
     };
+
+    using meta_domain = semilattice< trivial >;;
 
 } // namespace __lamp
