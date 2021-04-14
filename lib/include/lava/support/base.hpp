9069eb435bd3ec5014b5f5ab3409a763ada465e9
Henrich Lauko <xlauko@mail.muni.cz>
2021-04-14 09:30:19 +0200

    lava: get rid of obsolete tagged arrays

--- lib/include/lava/support/base.hpp
+++ lib/include/lava/support/base.hpp
@@ -19,119 +19,25 @@
 
 #include <cstdint>
 #include <string>
-
-#include <runtime/array.hpp>
+#include <memory>
+#include <limits>
 
 namespace __lava
 {
     using bitwidth_t = uint8_t;
-    using tag_t = uint8_t;
 
-    static constexpr tag_t invalid_tag = std::numeric_limits< tag_t >::max();
+    struct construct_shared_t {};
+    static constexpr construct_shared_t construct_shared; 
 
-    struct tagged_array : __lart::rt::array< uint8_t >
+    template< typename domain >
+    struct domain_ref : domain
     {
-        using base = __lart::rt::array< uint8_t >;
-        using typename base::iterator;
+        using base = typename domain::base;
 
-        using construct_shared_t = __lart::rt::construct_shared_t;
-
-        template< typename F >
-        void move_tag( F f )
-        {
-            uint8_t tag = base::back();
-            f();
-            base::back() = tag;
-        }
-
-        using base::base;
-
-        tagged_array() { base::resize( 1, invalid_tag ); }
-        tagged_array( const tagged_array &o, construct_shared_t s ) : base( o, s ) {}
-
-        template< typename derived >
-        derived storage_copy() const
-        {
-            tagged_array storage{ base::size(), base::begin(), base::end() };
-            return { storage.disown(), derived::construct_shared };
-        }
-
-        auto  end()       { return base::end() - 1; }
-        auto  end() const { return base::end() - 1; }
-        auto cend() const { return base::end() - 1; }
-
-        auto  rbegin()       { return base::rbegin() + 1; }
-        auto  rbegin() const { return base::rbegin() + 1; }
-        auto crbegin() const { return base::rbegin() + 1; }
-
-        auto       &back()       { return *( end() - 1 ); }
-        const auto &back() const { return *( end() - 1 ); }
-        uint8_t    &tag()        { return base::back(); }
-        uint8_t     tag()  const { return base::back(); }
-
-        unsigned size()  const { return base::size() - 1; }
-        bool    empty() const { return size() == 0; }
-        void    _grow() { base::push_back( 0 ); }
-
-        base       &raw()       { return *this; }
-        const base &raw() const { return *this; }
-
-        void *unsafe_ptr() const
-        {
-            auto ptr = static_cast< const void * >( &*base::begin() );
-            return const_cast< void * >( ptr );
-        }
-
-        void push_back( uint8_t v )          { move_tag( [&]{ base::back() = v; _grow(); } ); }
-        void emplace_back( uint8_t v )       { move_tag( [&]{ base::back() = v; _grow(); } ); }
-        void resize( unsigned sz )           { move_tag( [&]{ base::resize( sz + 1 ); } ); }
-        void clear()                         { move_tag( [&]{ base::resize( 1 ); } ); }
-        void pop_back()                      { move_tag( [&]{ base::pop_back(); } ); }
-        void insert( iterator i, uint8_t v ) { move_tag( [&]{ base::insert( i, v ); } ); }
-    };
-
-    template< typename type >
-    struct tagged_storage : tagged_array
-    {
-        using base = tagged_array;
-        using stored_t = type;
-
-        using construct_shared_t = __lart::rt::construct_shared_t;
-
-        tagged_storage( void *v, construct_shared_t s ) : base( v, s ) {}
-
-        template< typename... args_t >
-        tagged_storage( args_t && ... args )
-        {
-            resize( sizeof( type ) );
-            new ( &**this ) type( std::forward< args_t >( args ) ... );
-        }
-
-        tagged_storage( const tagged_storage &o ) : tagged_storage( o.get() ) {}
-        tagged_storage( tagged_storage &&o ) : tagged_storage( std::move( o.get() ) ) {}
-        tagged_storage &operator=( const tagged_storage &o ) { get() = o.get(); return *this; }
-
-        ~tagged_storage()
-        {
-            if ( begin() )
-                get().~type();
-        }
-
-        const type &operator*() const { return *reinterpret_cast< const type * >( begin() ); }
-        type       &operator*()       { return *reinterpret_cast< type * >( begin() ); }
-
-        const type *operator->() const { return &**this; }
-        type       *operator->()       { return &**this; }
-
-        type       &get()       { return **this; }
-        const type &get() const { return **this; }
-    };
-
-    template< typename base >
-    struct domain_ref : base
-    {
-        domain_ref( const base &v ) : base( v.unsafe_ptr(), __lart::rt::construct_shared ) {}
-        explicit domain_ref( void *v ) : base( v, __lart::rt::construct_shared ) {}
+        domain_ref( const domain_ref &v ) : domain( v.unsafe_ptr(), construct_shared ) {}
+        domain_ref( const domain     &v ) : domain( v.unsafe_ptr(), construct_shared ) {}
+        domain_ref( const base       &v ) : domain( v.unsafe_ptr(), construct_shared ) {}
+        explicit domain_ref( void    *v ) : domain( v             , construct_shared ) {}
 
         ~domain_ref() { this->disown(); }
     };
@@ -218,7 +124,6 @@ namespace __lava
         using index_dom = self_t;
         using scalar_dom = self_t;
 
-
         static inline st fail( const char *msg = "" )
         {
             // TODO log messgage
@@ -226,15 +131,10 @@ namespace __lava
             __builtin_trap();
         }
 
-        st clone() const
-        {
-            if constexpr ( std::is_copy_constructible_v< self_t > )
-            {
-                return st( static_cast< const self_t& >( *this ) );
-            }
+        self_t &      self()       { return *static_cast< self_t* >( this ); }
+        const self_t &self() const { return *static_cast< const self_t* >( this ); }
 
-            return fail( "clone" );
-        }
+        st clone() const { return { self().get() }; }
 
         static st lift_i1( i1 v )   { return st::lift( v ); }
         static st lift_i8( i8 v )   { return st::lift( v ); }
