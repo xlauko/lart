7d88216f11880eacf847ef74201f477c968ebe30
Henrich Lauko <xlauko@mail.muni.cz>
2021-09-05 11:42:15 +0200

    scripts: Add setup build script.

---
+++ scripts/setup.sh
@@ -0,0 +1,27 @@
+ cmake \
+    -GNinja \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DBUILD_LART_RUNTIME=ON \
+    -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
+    -DLIBCXX_INSTALL_DIR=${LIBCXX_DATAFLOW_DIR} \
+    -DCMAKE_TOOLCHAIN_FILE=${VCPKG_TOOLCHAIN} \
+    -DCMAKE_INSTALL_PREFIX=${LART_INSTALL_DIR} \
+    -B build-runtime \
+    -S .
+
+cmake --build build-runtime
+cmake --build build-runtime --target install
+
+cmake \
+    -GNinja \
+    -DBUILD_LART_CC=ON \
+    -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR}/build/ \
+    -DSVF_INSTALL_DIR=${SVF_INSTALL_DIR} \
+    -DCMAKE_TOOLCHAIN_FILE=${VCPKG_TOOLCHAIN} \
+    -DCMAKE_INSTALL_PREFIX=${LART_INSTALL_DIR} \
+    -B build \
+    -S .
+
+cmake --build build
+cmake --build build --target install
