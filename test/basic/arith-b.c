fe5da581d77abfcca3cece7c813c8ede7492794f
Henrich Lauko <xlauko@mail.muni.cz>
2021-09-30 08:49:45 +0000

    test: add term sign extend test

---
+++ test/basic/arith-b.c
@@ -0,0 +1,14 @@
+// RUN: %testrun %lartcc term -lz3 %s -o %t | %filecheck %s
+
+#include <lamp.h>
+#include <assert.h>
+
+#include "utils.h"
+
+int main() {
+    short a = __lamp_any_i16();
+    int b = __lamp_any_i32();
+    int r = a + b;
+    __lamp_dump(&r);
+    // CHECK: (bvadd ((_ sign_extend 16) var_1) var_2)
+}
