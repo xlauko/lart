714c4a9a814b9325f38281bbc78b3f3d8e3c16f0
Henrich Lauko <xlauko@mail.muni.cz>
2021-10-03 18:12:48 +0000

    test: add more boolean arithmetic tests

---
+++ test/basic/bool-arith-c.c
@@ -0,0 +1,17 @@
+// RUN: %testrun %lartcc term -lz3 %s -o %t | %filecheck %s
+// RUN: %testrun %lartcc unit %s -o %t | %filecheck %s
+
+#include <lamp.h>
+#include "utils.h"
+
+#include <stdbool.h>
+#include <assert.h>
+
+int main()
+{
+    bool a = __lamp_any_i1();
+    bool b = __lamp_any_i1();
+    int test = (int)a == b;
+    test += 1;
+    assert( test == 2 ); // CHECK: assertion test == 2 failed
+}
