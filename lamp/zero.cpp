97a717298695d67230bf780c92f1a7c1d62f92c3
hadamove <55924102+hadamove@users.noreply.github.com>
2021-10-07 18:25:07 +0200

    zero: import zero domain base

---
+++ lamp/zero.cpp
@@ -0,0 +1,9 @@
+#include <lava/zero.hpp>
+#include <lamp/support/storage.hpp>
+
+namespace __lamp
+{
+    using meta_domain = __lava::zero< wrapped_storage >;
+} // namespace __lamp
+
+#include "wrapper.hpp"
