b1feff6df2c8a90867c3d57b7acab022425d346e
Henrich Lauko <xlauko@mail.muni.cz>
2021-10-08 15:12:59 +0000

    test: add bool arith test

---
+++ test/basic/bool-arith-d.c
@@ -0,0 +1,21 @@
+// RUN: %testrun %lartcc term -lz3 %s -o %t | %filecheck %s
+
+#include <lamp.h>
+#include <assert.h>
+
+
+int nondet() {
+    return (int)__lamp_any_i32();
+}
+
+int main() {
+    int x = nondet();
+    if ( (short) x < 0 )
+        return 0;
+    short y = *(short *)( &x );
+    __lamp_dump(&y);
+    // CHECK: ((_ extract 15 0) var_1)
+    y++;
+    __lamp_dump(&y);
+    // CHECK: (bvadd ((_ extract 15 0) var_1) #x0001)
+}
