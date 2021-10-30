75ce523136d80dfa593d3606d02e0932a6693b73
Henrich Lauko <xlauko@mail.muni.cz>
2021-10-30 16:12:58 +0000

    lava: add operations enum

--- lava/include/lava/support/operations.hpp
+++ lava/include/lava/support/operations.hpp
@@ -16,8 +16,137 @@
 
 #pragma once
 
+#include <string_view>
+
 namespace __lava::op
 {
+    enum class tag
+    {
+        unknown,
+        any, lift, lower,
+        join, meet,
+        
+        alloca, store, load,
+
+        add, sub, mul, sdiv, udiv, srem, urem,
+        fadd, fsub, fmul, fdiv, frem,
+
+        fneg,
+        
+        op_and, op_or, op_xor,
+
+        zfit, zext, sext, trunc,
+
+        fptrunc, sitofp, uitofp, fptosi, fptoui, fpext,
+        inttoptr, ptrtoint,
+
+        ne, eq, slt, sgt, sle, sge, ult, ugt, ule, uge,
+
+        foeq, fogt, foge, folt, fole, fone, ford, 
+        funo, fueq, fugt, fuge, fult, fule, fune,
+
+        ffalse, ftrue,
+
+        shl, lshr, ashr, concat, extract,
+
+        malloc, realloc, dealloca, free
+    };
+
+    std::string_view to_string(tag t)
+    {
+        switch (t)
+        {
+            case tag::unknown: return "unknown";
+
+            case tag::any:   return "any";
+            case tag::lift:  return "lift";
+            case tag::lower: return "lower";
+            case tag::join:  return "join";
+            case tag::meet:  return "meet";
+            
+            case tag::alloca: return "alloca";
+            case tag::store:  return "store";
+            case tag::load:   return "load";
+
+            case tag::add: return "add";
+            case tag::sub: return "sub";
+            case tag::mul: return "mul";
+            case tag::sdiv: return "sdiv";
+            case tag::udiv: return "udiv";
+            case tag::srem: return "srem";
+            case tag::urem: return "urem";
+
+            case tag::fadd: return "fadd";
+            case tag::fsub: return "fsub";
+            case tag::fmul: return "fmul";
+            case tag::fdiv: return "fdiv";
+            case tag::frem: return "frem";
+
+            case tag::fneg: return "fneg";
+            
+            case tag::op_and: return "op_and";
+            case tag::op_or: return "op_or";
+            case tag::op_xor: return "op_xor";
+
+            case tag::zfit: return "zfit";
+            case tag::zext: return "zext"; 
+            case tag::sext: return "sext"; 
+            case tag::trunc: return "trunc";
+
+            case tag::fptrunc: return "fptrunc";
+            case tag::sitofp: return "sitofp";
+            case tag::uitofp: return "uitofp";
+            case tag::fptosi: return "fptosi";
+            case tag::fptoui: return "fptoui";
+            case tag::fpext: return "fpext";
+            case tag::inttoptr: return "inttoptr";
+            case tag::ptrtoint: return "ptrtoint";
+
+            case tag::ne: return "ne";
+            case tag::eq: return "eq";
+            case tag::slt: return "slt";
+            case tag::sgt: return "sgt";
+            case tag::sle: return "sle";
+            case tag::sge: return "sge";
+            case tag::ult: return "ult";
+            case tag::ugt: return "ugt";
+            case tag::ule: return "ule";
+            case tag::uge: return "uge";
+
+            case tag::foeq: return "foeq";
+            case tag::fogt: return "fogt";
+            case tag::foge: return "foge";
+            case tag::folt: return "folt";
+            case tag::fole: return "fole";
+            case tag::fone: return "fone";
+            case tag::ford: return "ford"; 
+            case tag::funo: return "funo";
+            case tag::fueq: return "fueq";
+            case tag::fugt: return "fugt";
+            case tag::fuge: return "fuge";
+            case tag::fult: return "fult";
+            case tag::fule: return "fule";
+            case tag::fune: return "fune";
+
+            case tag::ffalse: return "ffalse";
+            case tag::ftrue: return "ftrue";
+
+            case tag::shl: return "shl";
+            case tag::lshr: return "lshr";
+            case tag::ashr: return "ashr";
+            case tag::concat: return "concat";
+            case tag::extract: return "extract";
+
+            case tag::malloc: return "malloc";
+            case tag::realloc: return "realloc";
+            
+            case tag::dealloca: return "dealloca";
+            case tag::free : return "free";    
+        }
+
+        __builtin_unreachable();
+    }
+
     static constexpr auto lower  = []( const auto &a ) { return std::decay_t< decltype( a ) >::lower;  };
 
     static constexpr auto _alloca = []( const auto &a )
