ab658edd048ab919467a069250beb6f76b18a2d9
Henrich Lauko <xlauko@mail.muni.cz>
2021-11-11 17:06:39 +0000

    cc: report ignored instructions

--- lartcc/syntactic.cpp
+++ lartcc/syntactic.cpp
@@ -105,7 +105,14 @@ namespace lart
                     // TODO
                 }
             },
-            [&] ( llvm::Value * ) { /* fallthrough */ }
+            [&] ( llvm::Argument * ) { /* fallthrough */ },
+            [&] ( llvm::Value *value ) { 
+                /* fallthrough */
+                std::string buff;
+                llvm::raw_string_ostream ss(buff);
+                value->print(ss);
+                spdlog::info( "ignore abstract: {}", ss.str() );
+            }
         );
         return result;
     }
