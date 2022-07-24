d7730fdd64b793d296311440f81a6ef350bcb83e
Henrich Lauko <xlauko@mail.muni.cz>
2022-07-24 11:56:49 +0200

    ports: update svf port

--- ports/svf/portfile.cmake
+++ ports/svf/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO xlauko/SVF
-  REF 1de995e69219e900335cb4bd09b118332e8d7b9f
-  SHA512 09f43a99d9fe2395c03c834debc66c0edc2ef6b2184c476071f6219c6fc53794f35859a8f9497a67349f107b97fd5f71962c52e66d77c67a2d55101a9e3419c5
+  REF 76e14b2a51db9f6ece094975f049cfbbbc58347a
+  SHA512 ec2fd7d48ac64610f681ce24298c0208ecd0612ee8b0f663feca58f50a030ff9c8faa853d77a0b15165a995d041c6c6552cc8dce6fba170751b70f348986662e
   HEAD_REF main
 )
 
@@ -28,17 +28,26 @@ endif()
 set( LLVM_DIR "${LLVM_INSTALL_DIR}/lib/cmake/llvm/" CACHE PATH "LLVM config path" )
 message( STATUS "Using LLVM_DIR: ${LLVM_DIR}" )
 
+# Set this to a valid Z3 installation dir
+if ( DEFINED ENV{Z3_DIR} AND NOT DEFINED Z3_DIR )
+  set( Z3_DIR "$ENV{Z3_DIR}" )
+endif()
+
+set( Z3_DIR "" CACHE PATH "Z3 installation directory" )
+message( STATUS "Using Z3_DIR: ${Z3_DIR}" )
+
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
     -DLLVM_DIR=${LLVM_DIR}
+    -DZ3_DIR=${Z3_DIR}
     -DCMAKE_CXX_FLAGS=-Wno-unused-variable
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(
   PACKAGE_NAME "svf"
-  CONFIG_PATH lib/cmake/svf
+  CONFIG_PATH lib/cmake/SVF
 )
 
 file( REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" )
