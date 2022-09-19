b86f508d26331b2668d7799eceb1319e48026028
Pavol Misenko <pavolmisenko@gmail.com>
2022-09-19 12:16:21 +0200

    test: Svcomp benchmarks for interval domain evaluation.

--- test/interval/benchmarks/recursive/Fibonacci03.c
+++ test/interval/benchmarks/recursive/Fibonacci03.c
@@ -1,17 +1,31 @@
+<<<<<<< HEAD
 #include <svcomp.h>
 
+=======
+>>>>>>> 36dbba2 (test: Svcomp benchmarks for interval domain evaluation.)
 extern void abort(void);
 extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
 void reach_error() { __assert_fail("0", "Fibonacci03.c", 3, "reach_error"); }
 
 /*
  * Recursive computation of fibonacci numbers.
+<<<<<<< HEAD
  *
  * Author: Matthias Heizmann
  * Date: 2013-07-13
  *
  */
 
+=======
+ * 
+ * Author: Matthias Heizmann
+ * Date: 2013-07-13
+ * 
+ */
+
+extern int __VERIFIER_nondet_int(void);
+
+>>>>>>> 36dbba2 (test: Svcomp benchmarks for interval domain evaluation.)
 
 int fibonacci(int n) {
     if (n < 1) {
@@ -36,3 +50,7 @@ int main() {
         ERROR: {reach_error();abort();}
     }
 }
+<<<<<<< HEAD
+=======
+    
+>>>>>>> 36dbba2 (test: Svcomp benchmarks for interval domain evaluation.)
