e45dd14fe3b36b182a3cd37c4685ad330eff3ca2
Henrich Lauko <xlauko@mail.muni.cz>
2022-10-08 22:36:43 +0200

    test: add more memory tests

---
+++ test/basic/mem-d.c
@@ -0,0 +1,22 @@
+// RUN: %testrun %lartcc term -lz3 %s -o %t | %filecheck %s
+// REQUIRES: fix
+
+#include <lamp.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include "utils.h"
+
+int plus(int *a) {
+    *a += 1;
+    return *a;
+}
+
+int main() {
+    int *a;
+    int x = __lamp_any_i32();
+    a = &x;
+    int r = plus(a);
+    assert( x == r );
+    REACHABLE // CHECK-COUNT: lart-reachable
+}
