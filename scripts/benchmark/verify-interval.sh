6d7aaf493e5697489a7f20c5d5b32b08e48faa2a
Pavol Misenko <pavolmisenko@gmail.com>
2022-10-12 17:20:08 +0200

    scripts: Correct error outputs.

--- scripts/benchmark/verify-interval.sh
+++ scripts/benchmark/verify-interval.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-domain=$1
+file=$1
 
 trace_file="trace.out"
 
 export LART_NO_FAIL_MODE=ON
 export LART_TRACE_FILE=$trace_file
 
-lartcc interval ${@:2} -o abstracted
+lartcc interval file -o abstracted
 
 ./abstracted
 
 if [ -s $trace_file ]; then
-	echo  {\"result\": \"error\"} > /artefact/results.json
-    echo  {\"error_description\": \"`cat $trace_file`\"} >> /artefact/results.json
+	echo  {\"result\": \"error\", \"error_description\": \"`cat $trace_file`\"} > /artefact/results.json
 else
 	echo  {\"result\": \"no-error\"} > /artefact/results.json
 fi
