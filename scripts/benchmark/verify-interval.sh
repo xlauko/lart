9342d6a1a998f0d88e684ee1c9009e56a4e774b6
Pavol Misenko <pavolmisenko@gmail.com>
2022-10-12 13:33:07 +0200

    benchmark: Verification script just for interval domain.

---
+++ scripts/benchmark/verify-interval.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+domain=$1
+
+trace_file="trace.out"
+
+export LART_NO_FAIL_MODE=ON
+export LART_TRACE_FILE=$trace_file
+
+lartcc interval ${@:2} -o abstracted
+
+./abstracted
+
+if [ -s $trace_file ]; then
+	echo  {\"result\": \"error\"} > /artefact/results.json
+    echo  {\"error_description\": \"`cat $trace_file`\"} >> /artefact/results.json
+else
+	echo  {\"result\": \"no-error\"} > /artefact/results.json
+fi
+
